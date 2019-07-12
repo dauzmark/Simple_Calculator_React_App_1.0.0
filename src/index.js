dcde9742d55a0ac48ee461651c10c72128522b14
dauzmark <52662371+dauzmark@users.noreply.github.com>
2019-07-12 16:32:38 +0100

    edit
    edit 'AC' and 'C' functionality

--- src/index.js
+++ src/index.js
@@ -9,15 +9,27 @@ class App extends React.Component {
   state = {
     numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     operators: [".", "+", "-", "/", "*", "=", "AC", "C"],
-    output: []
+    // output: [],
+    output: [0]
   };
+
+  // numberClick = num => {
+  //   const output = [...this.state.output];
+
+  //   if (output.length === 0 && num === "0") {
+  //     console.log(output.length, output[0], num);
+  //     this.setState({ output: [] });
+  //   } else {
+  //     this.setState({ output: [...output, num] });
+  //   }
+  // };
 
   numberClick = num => {
     const output = [...this.state.output];
 
-    if (output.length === 0 && num === "0") {
+    if (output[0] === 0 && num !== "0") {
       console.log(output.length, output[0], num);
-      this.setState({ output: [] });
+      this.setState({ output: num });
     } else {
       this.setState({ output: [...output, num] });
     }
@@ -25,7 +37,7 @@ class App extends React.Component {
 
   operatorClick = operator => {
     if (operator === "AC" || operator === "C") {
-      this.setState({ output: "" });
+      this.setState({ output: [0] });
     } else if (operator === "=") {
       let output = this.calC();
       this.setState({ output: output });
